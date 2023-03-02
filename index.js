8b8364564781fe55293fb84880d536e4da0c34b7
admin <121937275+wusaldiko@users.noreply.github.com>
2023-03-02 18:05:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let modal = document.querySelector(".modal");
 let modalForm = modal.firstElementChild;
-let sendBtn = document.querySelector("#sendBtn");
+const $checkboxs = document.querySelectorAll(".modal__item-check")
+let btn = document.querySelector("#sendBtn");
 
 modalForm.addEventListener("click", (e) => {
   e.stopPropagation();
@@ -15,20 +16,11 @@ function hideModal() {
 }
 
 // ============================================
-let arr = [];
+window.name = "";
 
-function selectedCheckbox(checkbox) {
-  let isSelected = !!Number(checkbox.dataset.selected);
-  
-  if (!isSelected) {
-    arr.push(isSelected);
-    arr.length && (sendBtn.disabled = false);
-    checkbox.dataset.selected = 1;
-  } else {
-    arr.pop();
-    !arr.length && (sendBtn.disabled = true);
-    checkbox.dataset.selected = 0;
-  }
-}
-
+$checkboxs.forEach((el) =>
+  el.addEventListener("click", (_) => {
+    btn.disabled = !(el.checked ? window.name += "1": window.name = window.name.slice(1));
+  })
+);
     
